7dd15f0cfb60e1a2c4e91fb621ef1533db6310d1
Dor Rondel <dor.rondel@gmail.com>
2016-08-30 10:33:51 -0400

    added first day code

---
+++ day1/assignment1.py
@@ -0,0 +1,18 @@
+# Task 1
+# Print the following formatted as such:
+'''
+Twinkle, twinkle, little star,
+        How I wonder what you are!
+                Up above the world so high,
+                Like a diamond in the sky.
+Twinkle, twinkle, little star,
+        How I wonder what you are
+'''
+
+print("""Twinkle, twinkle, little star,
+\tHow I wonder what you are!
+\t\tUp above the world so high,
+\t\tLike a diamond in the sky.
+Twinkle, twinkle, little star,
+\tHow I wonder what you are
+""")
